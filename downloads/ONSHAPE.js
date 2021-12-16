23d5237d087df62013ab071eb829badd88661a9f
40823122 <40823122@gm.nfu.edu.tw>
2021-12-16 08:06:26 +0800

    w13零件進度"

---
+++ downloads/ONSHAPE.js
@@ -0,0 +1,14 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 498, y: 360, width: 60, height: 24 }, target: 3 },
+    ],
+    "521": [
+      { type: "gotoframe", rect: { x: 591, y: 210, width: 60, height: 24 }, target: 2 },
+    ],
+  },
+};
